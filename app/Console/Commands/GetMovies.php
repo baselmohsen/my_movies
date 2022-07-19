a25b0d350239709867cd8c534a85506a6b05006f
basel mohsen <105518530+baselmohsen@users.noreply.github.com>
2022-07-19 19:10:56 +0200

    Update GetMovies.php

--- app/Console/Commands/GetMovies.php
+++ app/Console/Commands/GetMovies.php
@@ -41,39 +41,15 @@ class GetMovies extends Command
      */
     public function handle()
     {
-       
-
-            $response = Http::get(https://api.themoviedb.org/3/movie/popular?region=us&api_key=00aad8be2a82e3a4d342cbc7d25afc24&page=1);
-
-            foreach ($response->json()['results'] as $result) {
-
-                $movie = Movie::updateOrCreate(
-                    [
-                        'e_id' => $result['id'],
-                        'title' => $result['title'],
-                    ],
-                    [
-                        'description' => $result['overview'],
-                        'poster' => $result['poster_path'],
-                        'banner' => $result['backdrop_path'],
-                        'release_date' => $result['release_date'],
-                        'vote' => $result['vote_average'],
-                        'vote_count' => $result['vote_count'],
-                    ]);
-
-                $this->attachGenres($result, $movie);
-
-                $this->attachActors($movie);
-
-                $this->getImages($movie);
-
-            }//end of for each
-
+        $this->getPopularMovies();
+        $this->getNowPlayingMovies();
+        $this->getUpcomingMovies();
 
     }//end of handle
 
     private function getPopularMovies()
     {
+        
         for ($i = 1; $i <= config('services.tmdb.max_pages'); $i++) {
 
             $response = Http::get(config('services.tmdb.base_url') . '/movie/popular?region=us&api_key=' . config('services.tmdb.api_key') . '&page=' . $i);
